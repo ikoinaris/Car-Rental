e3f11cac6eee5f46f76445567fb9ba1710dd6f50
ikoinaris <g.koinaris@hotmail.com>
2021-06-16 17:02:58 +0200

    sql file created for initializing data

---
+++ car-rental-api/car-rental/src/main/resources/templates/initial_data.sql
@@ -0,0 +1,13 @@
+/* initialize location values */
+INSERT INTO location (name) VALUES ('Airport');
+INSERT INTO location (name) VALUES ('Central Train Station');
+INSERT INTO location (name) VALUES ('Central Bus Station');
+INSERT INTO location (name) VALUES ('City Centre');
+INSERT INTO location (name) VALUES ('City Hall');
+
+/* initiazlize extra values */
+INSERT INTO extras (name, img_url, dailyPrice) VALUES ('Steal insurance', 'url', 10);
+INSERT INTO extras (name, img_url, dailyPrice) VALUES ('Baby sit', 'url', 15);
+INSERT INTO extras (name, img_url, dailyPrice) VALUES ('GPS', 'url', 20);
+
+/* initialize vehicle values */
