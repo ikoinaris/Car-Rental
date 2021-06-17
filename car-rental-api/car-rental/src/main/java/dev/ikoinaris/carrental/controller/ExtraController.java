ac251544872a5b02abba9101b9db0d418f658683
ikoinaris <g.koinaris@hotmail.com>
2021-06-17 16:15:26 +0200

    controllers of extras and locations updated with crossOrigin and requestMapping

--- car-rental-api/car-rental/src/main/java/dev/ikoinaris/carrental/controller/ExtraController.java
+++ car-rental-api/car-rental/src/main/java/dev/ikoinaris/carrental/controller/ExtraController.java
@@ -3,15 +3,19 @@ package dev.ikoinaris.carrental.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.ikoinaris.carrental.entity.Extra;
 import dev.ikoinaris.carrental.repository.ExtraRepository;
 
 @RestController
+@RequestMapping("/car-rental")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ExtraController {
 
     @Autowired
