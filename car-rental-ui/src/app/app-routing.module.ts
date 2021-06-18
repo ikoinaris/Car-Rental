42c3bc797d08eca313b8b016091457622ad320f6
ikoinaris <g.koinaris@hotmail.com>
2021-06-18 11:03:40 +0200

    buttons repositioning and routing updated

--- car-rental-ui/src/app/app-routing.module.ts
+++ car-rental-ui/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  { path: '', component: IntroComponent},
+  { path: '', component: CarRentComponent},
   { path: 'rent-a-car', component: CarRentComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent }
