08f856b296c41e7c12471aa00d781d6cb954cda3
ikoinaris <g.koinaris@hotmail.com>
2021-06-23 15:35:59 +0200

    vehicle controller get mapping updated

--- car-rental-api/car-rental/src/main/java/dev/ikoinaris/carrental/controller/VehicleController.java
+++ car-rental-api/car-rental/src/main/java/dev/ikoinaris/carrental/controller/VehicleController.java
@@ -4,10 +4,12 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.ikoinaris.carrental.entity.Vehicle;
@@ -15,6 +17,8 @@ import dev.ikoinaris.carrental.repository.VehicleRepository;
 import dev.ikoinaris.carrental.service.VehicleService;
 
 @RestController
+@RequestMapping("/car-rental")
+@CrossOrigin(origins = "http://localhost:4200")
 public class VehicleController {
 
     @Autowired
@@ -28,7 +32,7 @@ public class VehicleController {
         return vehicleRepository.findAll();
     }
 
-    @GetMapping("/available-vehicles")
+    @GetMapping("/available-cars")
     public List<Vehicle> getAvailableVehicles(@RequestBody String type, 
                                             @RequestBody String gear, 
                                             @RequestBody Date pickUpDate, 
